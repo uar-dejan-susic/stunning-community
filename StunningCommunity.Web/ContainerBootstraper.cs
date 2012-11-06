ebf444af6d735440eba1fe4f03a7f758d46a212a
Dejan Susic <dejo.susic@gmail.com>
2012-11-06 03:30:05 +0100

    Unity

---
+++ StunningCommunity.Web/ContainerBootstraper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Practices.ServiceLocation;
+using Unity.WebApi;
+
+namespace StunningCommunity.Web
+{
+    public class ContainerBootstraper
+    {
+        public static void Initalize()
+        {
+            var container = BuildContainer();
+
+            DependencyResolver.SetResolver( new UnityDependencyResolver( container ) );
+        }
+
+        public static IUnityContainer BuildContainer()
+        {
+            var container = new UnityContainer();
+
+            ServiceLocator.SetLocatorProvider( () => new UnityServiceLocator( container ) );
+
+            return container;
+        }
+    }
+}
